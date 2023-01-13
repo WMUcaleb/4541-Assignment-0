f9e1d823eec15feb44787ef77592377762cd2110
WMUcaleb <79730941+WMUcaleb@users.noreply.github.com>
2023-01-12 19:25:04 -0500

    Create simple_c_program.c

---
+++ simple_c_program.c
@@ -0,0 +1,13 @@
+// Date: 1/12/2023 
+// Class: CS4541
+// Assignment: Assignment 0
+// Author(s): Caleb Harrington
+ 
+#include <stdio.h>
+ 
+int main() {
+ 
+  printf("Hello, World!\n");
+ 
+  return 0;
+}
